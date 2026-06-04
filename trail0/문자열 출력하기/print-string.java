bd464313e7f844220d8691da6d2eabfae098d6f9
zero <tjdwn6@naver.com>
2026-06-04 11:26:28 +0900

    [Passed] 문자열 출력하기 (128ms, 10MB)

---
+++ trail0/문자열 출력하기/print-string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int i;
+
+        for(i = 0; i < N; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
