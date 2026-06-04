1c6f9d313c015db9a1b83f2bd6e19d127215ae9f
zero <tjdwn6@naver.com>
2026-06-04 11:23:49 +0900

    [Passed] 문자 출력하기 (126ms, 10MB)

---
+++ trail0/문자 출력하기/print-text.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+
+        int i;
+
+        for(i = 0; i < 8; i++) {
+            System.out.print(string);
+        }
+
+    }
+}
