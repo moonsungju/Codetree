c43c6c9ffd7cf89968f1b7820a464dff65905b26
zero <tjdwn6@naver.com>
2026-06-15 22:35:37 +0900

    [Passed] 따옴표 출력 (92ms, 9MB)

---
+++ trail0/따옴표 출력/print-quote.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String text = "He says \"It's a really simple sentence\".";
+        System.out.println(text);
+    }
+}
